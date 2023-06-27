2dcd500ad962c0a48d951d7d3f7398904a6fccc7
Michael Hunter <elec.mike@gmail.com>
2023-06-27 16:12:20 +1000

    minor

--- src/build-time.ts
+++ src/build-time.ts
@@ -1,11 +1,11 @@
 import { rootId } from "./id";
-import { currentPersona, definePersona } from "./persona";
+import { currentPersona, Persona } from "./persona";
 
 const callbacks: Array<() => void> = [];
 
 // It may be a bit of a hack to use a root ID here, but the build persona is
 // so universal to everything else that it seems like a reasonable choice.
-export const buildPersona = definePersona(rootId('build'), 'build', () => {
+export const buildPersona = new Persona(rootId('build'), 'build', () => {
   for (const callback of callbacks) {
     callback();
   }
